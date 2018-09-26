42201e5598ff2fecc34163bab60f7adb1c869984
Jannis Leifeld <jleifeld@meuter.de>
2018-09-26 09:43:26 +0200

    deploy

---
+++ precache-manifest.97d32b8cde7752b5ceeda822ae15b9d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vCard-Generator/robots.txt"
+  },
+  {
+    "revision": "246ae85391c4b3ae5909",
+    "url": "/vCard-Generator/js/chunk-vendors.f69071c9.js"
+  },
+  {
+    "revision": "37c2b64cbabab9877bac",
+    "url": "/vCard-Generator/js/app.a93bad64.js"
+  },
+  {
+    "revision": "1741b52a95223ef647eb0f16884b945f",
+    "url": "/vCard-Generator/index.html"
+  },
+  {
+    "revision": "37c2b64cbabab9877bac",
+    "url": "/vCard-Generator/css/app.b05c5a96.css"
+  }
+];
